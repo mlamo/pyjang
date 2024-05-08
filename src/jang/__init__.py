70b22df5bcb72866132ff27efa3551ba145e8f45
Mathieu Lamoureux <mathieu.lamoureux@uclouvain.be>
2024-05-08 14:33:28 +0200

    Towards 1.0

--- src/jang/__init__.py
+++ src/jang/__init__.py
@@ -1,4 +1,11 @@
+from pkg_resources import get_distribution, DistributionNotFound
 import logging
+
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    __version__ = "UNKNOWN"
 
 
 class DisableLogger:
