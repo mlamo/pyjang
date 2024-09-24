Computation optimisation and minor fixes
* Log-likelihood computation has been optimized, using vectorized features from ultranest and numpy.
* Reorganisation of the neutrinos IRFs in a more coherent way.
* Solved some dependency issues such as ultranest requiring numpy < 2.0.0 and python-ligo-lw not needed.
* Minor bug fixes.

examples/simple.py
@@ -23,7 +23,8 @@ import time
 from collections import defaultdict
 
 from momenta.io import GW, NuDetector, Parameters
-from momenta.io.neutrinos import BackgroundGaussian, BackgroundPoisson, EffectiveAreaAllSky
+from momenta.io.neutrinos import BackgroundGaussian, BackgroundPoisson
+from momenta.io.neutrinos_irfs import EffectiveAreaAllSky
 from momenta.stats.run import run_ultranest
 from momenta.stats.constraints import get_limits, get_limits_with_uncertainties
 from momenta.stats.bayes_factor import compute_log_bayes_factor_tobkg
@@ -96,7 +97,7 @@ def test_onesample(src, parameters):
 
     N = 20
     parameters.apply_det_systematics = False
-    parameters.prior_normalisation = "jeffreys-pois"
+    parameters.prior_normalisation = "jeffreys"
     for _ in range(N):
         t0 = time.time()
         model, result = run_ultranest(det, src, parameters)
@@ -106,55 +107,18 @@ def test_onesample(src, parameters):
         else:
             limit = get_limits(result["samples"], model)["flux0_norm"]
             unc = np.nan
-        results["jeffreyspois"].append(limit)
-        uncertainties["jeffreyspois"].append(unc)
-        times["jeffreyspois"] += time.time() - t0
+        results["jeffreys"].append(limit)
+        uncertainties["jeffreys"].append(unc)
+        times["jeffreys"] += time.time() - t0
 
     # compute naive upper limits
     nside = 8
     best_ipix = np.argmax(gw.fits.get_skymap(nside))
-    parameters.flux.set_shapes([2])
-    acc = EffAreaTest1().compute_acceptance(parameters.flux.components[0], best_ipix, nside)
+    acc = EffAreaTest1()._compute_acceptance(parameters.flux.components[0], best_ipix, nside)
     print(f"Naive UL = {2.3 / (acc/6):.2e}")
 
     for k in results.keys():
         print(f"{k:25s} => {np.average(results[k]):.2e} ± {np.std(results[k]):.2e} ({np.average(uncertainties[k]):.2e}), TIME = {times[k]/N:.2f} s")
-
-
-def test_bayesfactor(src, parameters):
-
-    det_str = """
-    name: TestDet
-    samples: ["A", "B"]
-    errors:
-        acceptance: 0.10
-        acceptance_corr: 0
-    """
-    det_file = f"{tmpdir}/detector.yaml"
-    with open(det_file, "w") as f:
-        f.write(det_str)
-
-    det = NuDetector(det_file)
-
-    class EffAreaTest1(EffectiveAreaAllSky):
-        def evaluate(self, energy, ipix, nside):
-            return energy**2 * np.exp(-energy / 10000)
-
-    det.set_effective_areas([EffAreaTest1(), EffAreaTest1()])
-    det.set_observations([0, 0], [BackgroundPoisson(20, 10), BackgroundPoisson(20, 10)])
-
-    N1 = np.arange(0, 10 + 1, 3)
-    N2 = np.arange(0, 10 + 1, 3)
-    N1, N2 = np.meshgrid(N1, N2, indexing="ij")
-    bf = np.zeros(N1.shape)
-
-    for i in range(N1.shape[0]):
-        for j in range(N2.shape[1]):
-            det.samples[0].nobserved = N1[i, j]
-            det.samples[1].nobserved = N2[i, j]
-            _, result = run_ultranest(det, src, parameters)
-            bf[i, j] = compute_log_bayes_factor_tobkg(result, det, src, parameters)
-            print(f"N1={N1[i, j]}, N2={N2[i, j]}, BF={bf[i, j]}")
 
 
 if __name__ == "__main__":
@@ -183,4 +147,3 @@ if __name__ == "__main__":
     gw.set_parameters(parameters)
 
     test_onesample(gw, parameters)
-    # test_bayesfactor(gw, parameters)

examples/superkamiokande.py
@@ -26,7 +26,8 @@ from scipy.interpolate import RegularGridInterpolator
 
 import momenta.utils.flux as flux
 from momenta.io import GWDatabase, NuDetector, Parameters
-from momenta.io.neutrinos import EffectiveAreaAltitudeDep, BackgroundFixed
+from momenta.io.neutrinos_irfs import EffectiveAreaAltitudeDep
+from momenta.io.neutrinos import BackgroundFixed
 from momenta.stats.run import run_ultranest
 from momenta.stats.constraints import get_limits
 
@@ -66,7 +67,7 @@ def single_event(gwname: str, gwdbfile: str, det_results: dict, pars: Parameters
 
     model, result = run_ultranest(sk, gw, pars)
     limits = get_limits(result["samples"], model, CL=0.90)
-    print("90% upper limit on flux normalisation:", limits["flux0_norm"])
+    print(f"90% upper limit on flux normalisation: {limits['flux0_norm']:.2e} /GeV/cm²")
 
 
 if __name__ == "__main__":

src/momenta/io/gw.py
@@ -30,6 +30,24 @@ import momenta.utils.conversions
 from momenta.io.transient import Transient
 
 
+default_samples_priorities = [
+    "PublicationSamples",
+    "Overall_posterior",
+    "C01:IMRPhenomXPHM",
+    "IMRPhenomXPHM",
+    "C01:IMRPhenomPv3HM",
+    "IMRPhenomPv3HM",
+    "C01:IMRPhenomPv2",
+    "IMRPhenomPv2",
+    "C01:IMRPhenomNSBH:HighSpin",
+    "IMRPhenomNSBH:HighSpin",
+    "C01:IMRPhenomNSBH:LowSpin",
+    "IMRPhenomNSBH:LowSpin",
+    "C01:Mixed",
+    "Mixed",
+]
+
+
 class GW(Transient):
     """Class to handle a full GW event, including FITS file (with skymap) and HDF5 file (with full posterior samples)."""
 
@@ -71,11 +89,11 @@ class GW(Transient):
             toys["distance_scaling"] = momenta.utils.conversions.distance_scaling(
                 toys.pop("luminosity_distance").to_numpy(), toys.pop("redshift").to_numpy()
             )
-            return toys
+            return toys.to_records(index=False)
         if self.fits:
-            return pd.DataFrame(data=self.fits.prepare_toys(nside))
+            return pd.DataFrame(data=self.fits.prepare_toys(nside)).to_records(index=False)
         self.log.warning("[GW] No toys are generated as this GW event has no FITS file nor posterior samples.")
-        return pd.DataFrame()
+        return pd.DataFrame().to_records(index=False)
 
 
 class _GWFits:
@@ -179,7 +197,7 @@ class _GWSamples:
         data = {}
         for var, varC in zip(variables, variables_corrected):
             data[var] = f[self.sample_name]["posterior_samples"][varC]
-            if var in ("ra", "deg"):
+            if var in ("ra", "dec"):
                 data[var] = np.rad2deg(data[var])  # convert from rad to deg
         f.close()
 

src/momenta/io/neutrinos.py
@@ -21,20 +21,17 @@ import logging
 import os
 import warnings
 
-import healpy as hp
 import numpy as np
 import scipy.integrate
 import yaml
-from scipy.integrate import quad, trapezoid
-from scipy.interpolate import interp1d, RegularGridInterpolator
 from scipy.linalg import block_diag
 from scipy.stats import gamma, truncnorm
 
 import astropy.coordinates
 import astropy.time
-from astropy.units import deg, rad
+from astropy.units import rad
 
-import momenta.stats.pdfs as pdf
+import momenta.io.neutrinos_irfs as irfs
 
 
 warnings.filterwarnings("ignore", category=scipy.integrate.IntegrationWarning)
@@ -63,125 +60,7 @@ def infer_uncertainties(input_array: float | np.ndarray, nsamples: int, correlat
     raise RuntimeError("The size of uncertainty_acceptance does not match with the number of samples")
 
 
-class EffectiveAreaBase:
-    """Class to handle detector effective area for a given sample and neutrino flavour.
-    This default class handles only energy-dependent effective area."""
-
-    def __init__(self):
-        self.acceptances = {}
-
-    def evaluate(self, energy: float | np.ndarray, ipix: int, nside: int):
-        return 0
-
-    def compute_acceptance(self, fluxcomponent, ipix: int, nside: int):
-        def func(x: float):
-            return fluxcomponent.evaluate(np.exp(x)) * self.evaluate(np.exp(x), ipix, nside) * np.exp(x)
-
-        return quad(func, np.log(fluxcomponent.emin), np.log(fluxcomponent.emax), limit=500)[0]
-
-    def compute_acceptance_map(self, fluxcomponent, nside: int):
-        return np.array([self.compute_acceptance(fluxcomponent, ipix, nside) for ipix in range(hp.nside2npix(nside))])
-
-    def get_acceptance_map(self, fluxcomponent, nside: int):
-        if fluxcomponent.store_acceptance:
-            if str(fluxcomponent) not in self.acceptances:
-                self.acceptances[str(fluxcomponent)] = self.compute_acceptance_map(fluxcomponent, nside)
-            return hp.ud_grade(self.acceptances[str(fluxcomponent)], nside)
-        return self.compute_acceptance_map(fluxcomponent, nside)
-
-    def get_acceptance(self, fluxcomponent, ipix: int, nside: int):
-        if fluxcomponent.store_acceptance:
-            return self.get_acceptance_map(fluxcomponent, nside)[ipix]
-        return self.compute_acceptance(fluxcomponent, ipix, nside)
-
-
-class EffectiveAreaAllSky(EffectiveAreaBase):
-
-    def __init__(self):
-        super().__init__()
-        self.func = None
-
-    def read_csv(self, csvfile: str):
-        x, y = np.loadtxt(csvfile, delimiter=",").T
-        self.func = interp1d(x, y, bounds_error=False, fill_value=0)
-
-    def evaluate(self, energy: float | np.ndarray, ipix: int, nside: int):
-        return self.func(energy)
-
-    def compute_acceptance_map(self, fluxcomponent, nside):
-        acc = self.compute_acceptance(fluxcomponent, 0, nside) * np.ones(hp.nside2npix(nside))
-        return acc
-
-
-class EffectiveAreaDeclinationDep(EffectiveAreaBase):
-
-    def __init__(self):
-        super().__init__()
-        self.mapping = {}
-
-    def evaluate(self, energy: float | np.ndarray, ipix: int, nside: int):
-        if nside not in self.mapping:
-            self.mapping[nside] = self.map_ipix_to_declination(nside)
-        return self.func(energy, self.mapping[nside][ipix])
-
-    def compute_acceptance_map(self, fluxcomponent, nside):
-        if nside not in self.mapping:
-            self.mapping[nside] = self.map_ipix_to_declination(nside)
-        acc = np.zeros(hp.nside2npix(nside))
-        for dec, ipix in zip(*np.unique(self.mapping[nside], return_index=True)):
-            acc[self.mapping[nside] == dec] = self.compute_acceptance(fluxcomponent, ipix, nside)
-        return acc
-
-    def map_ipix_to_declination(self, nside):
-        ipix = np.arange(hp.nside2npix(nside))
-        _, dec = hp.pix2ang(nside, ipix, lonlat=True)
-        return dec
-
-
-class EffectiveAreaAltitudeDep(EffectiveAreaBase):
-
-    def __init__(self):
-        super().__init__()
-        self.func = None
-        self.mapping = {}
-
-    def read(self):
-        bins_logenergy = ...  # shape (M,)
-        bins_altitude = ...  # shape (N,)
-        aeff = ...  # shape (M,N)
-        self.func = RegularGridInterpolator((bins_logenergy, bins_altitude), aeff, bounds_error=False, fill_value=0)
-
-    def set_location(self, time, lat_deg, lon_deg):
-        self.obstime = time
-        self.location = astropy.coordinates.EarthLocation(lat=lat_deg * deg, lon=lon_deg * deg)
-        self.mapping = {}
-
-    def evaluate(self, energy: float | np.ndarray, ipix: int, nside: int):
-        if nside not in self.mapping:
-            self.mapping[nside] = self.map_ipix_to_altitude(nside)
-        return self.func((np.log10(energy), self.mapping[nside][ipix]))
-
-    def compute_acceptance_map(self, fluxcomponent, nside):
-        if nside not in self.mapping:
-            self.mapping[nside] = self.map_ipix_to_altitude(nside)
-        acc = np.zeros(hp.nside2npix(nside))
-        for alt, ipix in zip(*np.unique(self.mapping[nside], return_index=True)):
-            acc[self.mapping[nside] == alt] = self.compute_acceptance(fluxcomponent, ipix, nside)
-        return acc
-
-    def map_ipix_to_altitude(self, nside):
-        ipix = np.arange(hp.nside2npix(nside))
-        ra, dec = hp.pix2ang(nside, ipix, lonlat=True)
-        coords_eq = astropy.coordinates.SkyCoord(ra=ra * deg, dec=dec * deg, frame="icrs")
-        coords_loc = coords_eq.transform_to(astropy.coordinates.AltAz(obstime=self.obstime, location=self.location))
-        return coords_loc.alt.deg
-
-
 class Background(abc.ABC):
-    @abc.abstractmethod
-    def prepare_toys(self, ntoys: int):
-        pass
-
     @property
     @abc.abstractmethod
     def nominal(self):
@@ -200,9 +79,6 @@ class BackgroundFixed(Background):
     def __init__(self, b0: float):
         self.b0 = b0
 
-    def prepare_toys(self, ntoys: int):
-        return self.b0 * np.ones(ntoys)
-
     @property
     def nominal(self):
         return self.b0
@@ -217,9 +93,7 @@ class BackgroundFixed(Background):
 class BackgroundGaussian(Background):
     def __init__(self, b0: float, error_b: float):
         self.b0, self.error_b = b0, error_b
-
-    def prepare_toys(self, ntoys: int):
-        return truncnorm.rvs(-self.b0 / self.error_b, np.inf, loc=self.b0, scale=self.error_b, size=ntoys)
+        self.func = truncnorm(-self.b0 / self.error_b, np.inf, loc=self.b0, scale=self.error_b)
 
     @property
     def nominal(self):
@@ -229,25 +103,23 @@ class BackgroundGaussian(Background):
         return f"{self.b0:.2e} +/- {self.error_b:.2e}"
 
     def prior_transform(self, x):
-        return truncnorm.ppf(x, -self.b0 / self.error_b, np.inf, loc=self.b0, scale=self.error_b)
+        return self.func.ppf(x)
 
 
 class BackgroundPoisson(Background):
     def __init__(self, Noff: int, alpha_offon: int):
         self.Noff, self.alpha_offon = Noff, alpha_offon
-
-    def prepare_toys(self, ntoys: int):
-        return gamma.rvs(self.Noff + 1, scale=1 / self.alpha_offon, size=ntoys)
+        self.func = gamma(self.Noff + 1, scale=1 / self.alpha_offon)
 
     @property
     def nominal(self):
         return self.Noff / self.alpha_offon
 
     def __repr__(self):
-        return f"{self.nominal:.2e} = {self.Noff:d}/{self.alpha_offon:.2e}"
+        return f"{self.nominal:.2e} = {self.Noff:.0f}/{self.alpha_offon:.2e}"
 
     def prior_transform(self, x):
-        return gamma.ppf(x, self.Noff + 1, scale=1 / self.alpha_offon)
+        return self.func.ppf(x)
 
 
 class NuEvent:
@@ -308,8 +180,8 @@ class NuSample:
         self.effective_area = None
         self.nobserved = np.nan
         self.background = None
-        self.events = None
-        self.pdfs = {
+        self._events = None
+        self._pdfs = {
             "signal": {"ang": None, "ene": None, "time": None},
             "background": {"ang": None, "ene": None, "time": None},
         }
@@ -322,16 +194,21 @@ class NuSample:
         self.background = bkg
 
     def set_events(self, events: list[NuEvent]):
-        self.events = events
+        assert len(events) == self.nobserved or events is None
+        self._events = events
+
+    @property
+    def events(self):
+        return self._events
 
     def set_pdfs(
         self,
-        sig_ang: pdf.AngularSignal | None = None,
-        sig_ene: pdf.EnergySignal | None = None,
-        sig_time: pdf.TimeSignal | None = None,
-        bkg_ang: pdf.AngularBackground | None = None,
-        bkg_ene: pdf.EnergyBackground | None = None,
-        bkg_time: pdf.TimeBackground | None = None,
+        sig_ang: irfs.AngularSignal | None = None,
+        sig_ene: irfs.EnergySignal | None = None,
+        sig_time: irfs.TimeSignal | None = None,
+        bkg_ang: irfs.AngularBackground | None = None,
+        bkg_ene: irfs.EnergyBackground | None = None,
+        bkg_time: irfs.TimeBackground | None = None,
     ):
         if (sig_ang is None) ^ (bkg_ang is None):
             raise RuntimeError("One of the angular PDFs is missing!")
@@ -339,24 +216,28 @@ class NuSample:
             raise RuntimeError("One of the energy PDFs is missing!")
         if (sig_time is None) ^ (bkg_time is None):
             raise RuntimeError("One of the time PDFs is missing!")
-        self.pdfs["signal"]["ang"] = sig_ang
-        self.pdfs["signal"]["ene"] = sig_ene
-        self.pdfs["signal"]["time"] = sig_time
-        self.pdfs["background"]["ang"] = bkg_ang
-        self.pdfs["background"]["ene"] = bkg_ene
-        self.pdfs["background"]["time"] = bkg_time
+        self._pdfs["signal"]["ang"] = sig_ang
+        self._pdfs["signal"]["ene"] = sig_ene
+        self._pdfs["signal"]["time"] = sig_time
+        self._pdfs["background"]["ang"] = bkg_ang
+        self._pdfs["background"]["ene"] = bkg_ene
+        self._pdfs["background"]["time"] = bkg_time
 
-    def compute_event_probability(self, nsigs, nbkg, ev, ra_src, dec_src, flux, nobkg=False):
-        psig, pbkg = np.ones_like(nsigs), 1
-        if self.pdfs["signal"]["ang"] is not None and self.pdfs["background"]["ang"]:
-            psig *= self.pdfs["signal"]["ang"](ev, ra_src, dec_src)
-            pbkg *= self.pdfs["background"]["ang"](ev)
-        if self.pdfs["signal"]["ene"] is not None and self.pdfs["background"]["ene"]:
-            psig *= self.pdfs["signal"]["ene"](ev, flux)
-            pbkg *= self.pdfs["background"]["ene"](ev)
-        if nobkg:
-            return (psig.dot(nsigs) + (nbkg - np.sum(nsigs)) * pbkg) / nbkg
-        return (psig.dot(nsigs) + nbkg * pbkg) / (np.sum(nsigs) + nbkg)
+    def compute_background_probability(self, ev):
+        pbkg = 1
+        if self._pdfs["background"]["ang"] is not None:
+            pbkg *= self._pdfs["background"]["ang"](ev)
+        if self._pdfs["background"]["ene"] is not None:
+            pbkg *= self._pdfs["background"]["ene"](ev)
+        return pbkg
+
+    def compute_signal_probability(self, ev, fluxcomponent, ra_src, dec_src):
+        psig = 1
+        if self._pdfs["signal"]["ang"] is not None:
+            psig *= self._pdfs["signal"]["ang"](ev, ra_src, dec_src)
+        if self._pdfs["signal"]["ene"] is not None:
+            psig *= self._pdfs["signal"]["ene"](ev, fluxcomponent)
+        return psig
 
 
 class NuDetectorBase(abc.ABC):
@@ -425,7 +306,7 @@ class NuDetector(NuDetectorBase):
         for i, smp in enumerate(self.samples):
             smp.set_observations(nobserved[i], background[i])
 
-    def set_effective_areas(self, aeffs: list[EffectiveAreaBase]):
+    def set_effective_areas(self, aeffs: list[irfs.EffectiveAreaBase]):
         for i, smp in enumerate(self.samples):
             smp.set_effective_area(aeffs[i])
 

src/momenta/io/transient.py
@@ -80,4 +80,4 @@ class PointSource(Transient):
             toys["ra"] = self.coords.ra.deg + np.rad2deg(dra)
             toys["dec"] = self.coords.dec.deg + np.rad2deg(ddec)
         toys["ipix"] = hp.ang2pix(nside, toys["ra"], toys["dec"], lonlat=True)
-        return pd.DataFrame(data=toys)
+        return pd.DataFrame(data=toys).to_records(index=False)

src/momenta/stats/model.py
@@ -30,30 +30,30 @@ def calculate_deterministics(samples, model):
     """
     det = {}
     # Stop here if distance is not provided
-    if "distance_scaling" not in model.toys_src.columns:
+    if "distance_scaling" not in model.toys_src.dtype.names:
         return det
-    itoys = samples["itoy"]
+    itoys = samples["itoy"].astype(int)
     nsamples = len(itoys)
-    distance_scaling = model.toys_src.iloc[itoys]["distance_scaling"].to_numpy()
+    distance_scaling = model.toys_src[itoys]["distance_scaling"]
     norms = np.array([samples[f"flux{i}_norm"] for i in range(model.flux.ncomponents)])
-    if model.flux.nshapes > 0:
+    if model.flux.nshapevars > 0:
         shapes = np.array([samples[f"flux{i}_{s}"] for i, c in enumerate(model.flux.components) for s in c.shape_names])
         det["eiso"] = np.empty(nsamples)
         for isample in range(nsamples):
-            model.flux.set_shapes(shapes[:, isample])
+            model.flux.set_shapevars(shapes[:, isample])
             det["eiso"][isample] = np.sum(norms * model.flux.flux_to_eiso(distance_scaling[isample]))
     else:
         det["eiso"] = np.sum(norms + model.flux.flux_to_eiso(distance_scaling), axis=0)
-    if "radiated_energy" in model.toys_src.columns:
-        radiated_energy = model.toys_src.iloc[itoys]["radiated_energy"].to_numpy()
+    if "radiated_energy" in model.toys_src.dtype.names:
+        radiated_energy = model.toys_src[itoys]["radiated_energy"]
         det["fnuiso"] = det["eiso"] / (radiated_energy * solarmass_to_erg)
     # Stop here if jet model is not provided or if `theta_jn`` is missing
-    if model.parameters.jet is None or "theta_jn" not in model.toys_src.columns:
+    if model.parameters.jet is None or "theta_jn" not in model.toys_src.dtype.names:
         return det
-    theta_jn = model.toys_src.iloc[itoys]["theta_jn"].to_numpy()
+    theta_jn = model.toys_src[itoys]["theta_jn"]
     det["etot"] = det["eiso"] / model.parameters.jet.etot_to_eiso(theta_jn)
-    if "radiated_energy" in model.toys_src.columns:
-        radiated_energy = model.toys_src.iloc[itoys]["radiated_energy"].to_numpy()
+    if "radiated_energy" in model.toys_src.dtype.names:
+        radiated_energy = model.toys_src[itoys]["radiated_energy"]
         det["fnu"] = det["etot"] / (radiated_energy * solarmass_to_erg)
     return det
 
@@ -88,7 +88,7 @@ class ModelNested:
     @property
     def param_names(self):
         params = [f"flux{i}_norm" for i in range(self.flux.ncomponents)]
-        params += [f"flux{i}_{s}" for i, c in enumerate(self.flux.components) for s in c.shape_names]
+        params += [f"flux{i}_{s}" for i, c in enumerate(self.flux.components) for s in c.shapevar_names]
         params += ["itoy"]
         if self.bkg_variations:
             params += [f"bkg{i}" for i in range(self.nsamples)]  # background
@@ -109,8 +109,8 @@ class ModelNested:
         i = 0
         x[i : i + self.flux.ncomponents] = self.prior_norm(x[i : i + self.flux.ncomponents])
         i += self.flux.ncomponents
-        x[i : i + self.flux.nshapes] = self.flux.prior_transform(x[i : i + self.flux.nshapes])
-        i += self.flux.nshapes
+        x[i : i + self.flux.nshapevars] = self.flux.prior_transform(x[i : i + self.flux.nshapevars])
+        i += self.flux.nshapevars
         x[i] = np.floor(self.ntoys_src * x[i])
         i += 1
         if self.bkg_variations:
@@ -122,13 +122,31 @@ class ModelNested:
             x[i : i + self.nsamples] = np.ones(self.nsamples) + np.dot(self.chol_cov_acc, rvs)
         return x
 
+    def prior_vec(self, cube):
+        x = cube.copy()
+        i = 0
+        x[..., i : i + self.flux.ncomponents] = self.prior_norm(x[..., i : i + self.flux.ncomponents])
+        i += self.flux.ncomponents
+        x[..., i : i + self.flux.nshapevars] = self.flux.prior_transform(x[..., i : i + self.flux.nshapevars])
+        i += self.flux.nshapevars
+        x[..., i] = np.floor(self.ntoys_src * x[..., i])
+        i += 1
+        if self.bkg_variations:
+            for j in range(self.nsamples):
+                x[..., i + j] = self.bkg[j].prior_transform(x[..., i + j])
+            i += self.nsamples
+        if self.acc_variations:
+            rvs = norm.ppf(x[..., i : i + self.nsamples])
+            x[..., i : i + self.nsamples] = np.ones(self.nsamples) + np.dot(rvs, self.chol_cov_acc)
+        return x
+
     def loglike(self, cube):
         # Format input parameters
         i = 0
         norms = cube[i : i + self.flux.ncomponents]
         i += self.flux.ncomponents
-        shapes = cube[i : i + self.flux.nshapes]
-        i += self.flux.nshapes
+        shapes = cube[i : i + self.flux.nshapevars]
+        i += self.flux.nshapevars
         itoy = int(np.floor(cube[i]))
         i += 1
         if self.bkg_variations:
@@ -141,24 +159,28 @@ class ModelNested:
         else:
             facc = 1
         # Get acceptance
-        if self.flux.nshapes > 0:
-            self.flux.set_shapes(shapes)
-        toy = self.toys_src.iloc[itoy]
+        if self.flux.nshapevars > 0:
+            self.flux.set_shapevars(shapes)
+        toy = self.toys_src[itoy]
         acc = np.array(
-            [
-                [s.effective_area.get_acceptance(c, int(toy["ipix"]), self.parameters.nside) for s in self.detector.samples]
-                for c in self.flux.components
-            ]
+            [[s.effective_area.get_acceptance(c, toy.ipix, self.parameters.nside) for s in self.detector.samples] for c in self.flux.components]
         )
         # Compute log-likelihood
-        nsigs = facc * np.array(norms)[:, np.newaxis] * acc / 6
-        loglkl = np.sum(poisson.logpmf(self.nobs, nbkg + np.sum(nsigs, axis=0)))
+        nsigs = facc * np.array(norms)[:, np.newaxis] * acc / 6  # (ncompflux, nsamples)
+        nexps = nbkg + np.sum(nsigs, axis=0)
+        if self.parameters.likelihood_method == "poisson":
+            loglkl = -np.sum(nexps + self.nobs * np.log(nexps))
         if self.parameters.likelihood_method == "pointsource":
+            loglkl = -np.sum(nexps)
             for i, s in enumerate(self.detector.samples):
                 if s.events is None:
+                    loglkl += self.nobs[i] * np.log(nexps[i])
                     continue
                 for ev in s.events:
-                    loglkl += np.log(s.compute_event_probability(nsigs[:, i], nbkg[i], ev, toy["ra"], toy["dec"], self.flux))
+                    pbkg = s.compute_background_probability(ev)
+                    psig = np.array([s.compute_signal_probability(ev, c, toy.ra, toy.dec) for c in self.flux.components])
+                    loglkl += np.log(nbkg[i] * pbkg + np.sum(nsigs[:, i] * psig))
+
         # Add Jeffreys' prior
         if self.fluxnorm_prior == "jeffreys":
             m_acc = facc * acc / 6  # shape = (ncomps, nsamples)
@@ -166,6 +188,68 @@ class ModelNested:
             m_fisher = np.matmul(m_acc / m_nexp, (m_acc / m_nexp).T)  # shape = (ncomps, ncomps)
             det_fisher = np.linalg.det(m_fisher)
             loglkl -= 0.5 * np.log(det_fisher)
+        return loglkl
+
+    def loglike_vec(self, cube):
+        npoints = cube.shape[0]
+        # INPUTS
+        # > flux normalisation parameters
+        i = 0
+        norms = cube[:, i : i + self.flux.ncomponents]  # dims = (npoints, ncompflux)
+        i += self.flux.ncomponents
+        # > flux shape parameters
+        shapes = cube[:, i : i + self.flux.nshapevars]  # dims = (npoints, nshapes)
+        i += self.flux.nshapevars
+        # > source parameter
+        itoys = np.floor(cube[:, i]).astype(int)
+        toys = self.toys_src[itoys]
+        i += 1
+        # > background parameters
+        if self.bkg_variations:
+            nbkg = cube[:, i : i + self.nsamples]  # dims = (npoints, nsamples)
+            i += self.nsamples
+        else:
+            nbkg = np.tile([b.nominal for b in self.bkg], (npoints, 1))
+        # > acceptance variation parameters
+        if self.acc_variations:
+            facc = cube[:, i : i + self.nsamples]  # dims = (npoints, nsamples)
+        else:
+            facc = np.ones((npoints, self.nsamples))
+        # ACCEPTANCE
+        accs = np.zeros((npoints, self.flux.ncomponents, self.detector.nsamples))  # dims = (npoints, ncompflux, nsamples)
+        for ipoint in range(npoints):
+            ishape = 0
+            for iflux, c in enumerate(self.flux.components):
+                if c.nshapevars > 0:
+                    c.set_shapevars(shapes[ipoint, ishape : ishape + c.nshapevars])
+                    ishape += c.nshapevars
+                for isample, s in enumerate(self.detector.samples):
+                    accs[ipoint, iflux, isample] = s.effective_area.get_acceptance(c, toys[ipoint].ipix, self.parameters.nside)
+        # LOG-LIKELIHOOD
+        nsigs = facc[:, np.newaxis, :] * (norms[:, :, np.newaxis] * accs / 6)  # dims = (npoints, ncompflux, nsamples)
+        nexps = nbkg + np.sum(nsigs, axis=1)  # dims = (npoints, nsamples)
+        if self.parameters.likelihood_method == "poisson":
+            loglkl = -np.sum(nexps + self.nobs * np.log(nexps), axis=1)  # dims = (npoints, )
+        if self.parameters.likelihood_method == "pointsource":
+            loglkl = -np.sum(nexps, axis=1)  # dims = (npoints, )
+            for isample, s in enumerate(self.detector.samples):
+                if s.events is None:
+                    loglkl += self.nobs[isample] * np.log(nexps[:, isample])  # dims = (npoints, )
+                    continue
+                psigs = np.zeros((npoints, self.flux.ncomponents, s.nobserved))  # dims = (npoints, ncompflux, nevents)
+                ishape = 0
+                for iflux, c in enumerate(self.flux.components):
+                    for ipoint in range(npoints):
+                        if c.nshapevars > 0:
+                            c.set_shapevars(shapes[ipoint, ishape : ishape + c.nshapevars])
+                        for ievt, evt in enumerate(s.events):
+                            psigs[ipoint, iflux, ievt] = s.compute_signal_probability(evt, c, toys[ipoint].ra, toys[ipoint].dec)
+                    ishape += c.nshapevars
+                pbkgs = np.zeros(s.nobserved)
+                for ievt, evt in enumerate(s.events):
+                    pbkgs[ievt] = s.compute_background_probability(evt)
+                probs = nbkg[:, isample, np.newaxis] * pbkgs + np.sum(nsigs[:, :, isample, np.newaxis] * psigs, axis=1)
+                loglkl += np.sum(np.log(probs), axis=1)
         return loglkl
 
 